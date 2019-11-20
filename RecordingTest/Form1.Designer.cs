daaba8820ed3a4dc57f5884de4d94d8ebbd8d673
nourhan <nourhan.bchr@gmail.com>
2019-11-20 14:38:52 +0200

    Add project files.

---
+++ RecordingTest/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace RecordingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.videoView2 = new LibVLCSharp.WinForms.VideoView();
+            this.videoView3 = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Location = new System.Drawing.Point(12, 12);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(411, 280);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // videoView2
+            // 
+            this.videoView2.BackColor = System.Drawing.Color.Black;
+            this.videoView2.Location = new System.Drawing.Point(451, 12);
+            this.videoView2.MediaPlayer = null;
+            this.videoView2.Name = "videoView2";
+            this.videoView2.Size = new System.Drawing.Size(391, 280);
+            this.videoView2.TabIndex = 1;
+            this.videoView2.Text = "videoView2";
+            // 
+            // videoView3
+            // 
+            this.videoView3.BackColor = System.Drawing.Color.Black;
+            this.videoView3.Location = new System.Drawing.Point(883, 12);
+            this.videoView3.MediaPlayer = null;
+            this.videoView3.Name = "videoView3";
+            this.videoView3.Size = new System.Drawing.Size(391, 280);
+            this.videoView3.TabIndex = 2;
+            this.videoView3.Text = "videoView3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1649, 555);
+            this.Controls.Add(this.videoView3);
+            this.Controls.Add(this.videoView2);
+            this.Controls.Add(this.videoView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private LibVLCSharp.WinForms.VideoView videoView2;
+        private LibVLCSharp.WinForms.VideoView videoView3;
+    }
+}
+
---
+++ RecordingTest/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+using System.IO;
+using System.Reflection;
+
+namespace RecordingTest
+{
+    public partial class Form1 : Form
+    {
+        const string VIDEO_URL1 = "rtsp://192.168.0.91/axis-media/media.amp";//
+        const string VIDEO_URL2 = "rtsp://admin:admin@192.168.1.168/defaultPrimary?streamtype=u";
+        const string VIDEO_URL3 = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+        readonly LibVLC _libvlc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Record in a file "record.ts" located in the bin folder next to the app
+            var currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var destination1 = Path.Combine(currentDirectory, "record.ts");
+            var destination2 = Path.Combine(currentDirectory, "record2.ts");
+            var destination3 = Path.Combine(currentDirectory, "record3.ts");
+
+            // this will load the native libvlc library (if needed, depending on the platform). 
+            Core.Initialize();
+
+            // instanciate the main libvlc object
+            _libvlc = new LibVLC();
+
+            // Redirect log output to the console
+            _libvlc.Log += (sender, e) => Console.WriteLine($"[{e.Level}] {e.Module}:{e.Message}");
+
+            // Create new media with HLS link
+            Media media = new Media(_libvlc, VIDEO_URL1, FromType.FromLocation);
+            Media media2 = new Media(_libvlc, VIDEO_URL2, FromType.FromLocation);
+            Media media3 = new Media(_libvlc, VIDEO_URL3, FromType.FromLocation);
+
+            // Define stream output options. 
+            // In this case stream to a file with the given path and play locally the stream while streaming it.
+            media.AddOption(":sout=#transcode{scodec=none}:duplicate{dst=display,dst=std{access=file,mux=ts,dst='" + destination1 + "'}}");
+            media.AddOption(":no-sout-all:sout-keep");
+            media2.AddOption(":sout=#transcode{scodec=none}:duplicate{dst=display,dst=std{access=file,mux=ts,dst='" + destination2 + "'}}");
+            media2.AddOption(":no-sout-all:sout-keep");
+            media3.AddOption(":sout=#transcode{scodec=none}:duplicate{dst=display,dst=std{access=file,mux=ts,dst='" + destination3 + "'}}");
+            media3.AddOption(":no-sout-all:sout-keep");
+
+            // Start recording
+            videoView1.MediaPlayer = new MediaPlayer(_libvlc);
+            videoView1.MediaPlayer.Play(media);
+            videoView2.MediaPlayer = new MediaPlayer(_libvlc);
+            videoView2.MediaPlayer.Play(media2);
+            videoView3.MediaPlayer = new MediaPlayer(_libvlc);
+            videoView3.MediaPlayer.Play(media3);
+        }
+    }
+}
